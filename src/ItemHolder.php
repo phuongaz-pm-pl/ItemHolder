a386dea970cc766569819b481117eaeac4f56873
Nguyen Tan Phuong <37770098+Phuongaz@users.noreply.github.com>
2023-09-12 00:29:38 +0700

    Remove test function

--- src/ItemHolder.php
+++ src/ItemHolder.php
@@ -12,7 +12,6 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\SingletonTrait;
 use poggit\libasynql\libasynql;
-use function _PHPStan_a4fa95a42\RingCentral\Psr7\str;
 
 class ItemHolder extends PluginBase {
     use SingletonTrait;
@@ -29,14 +28,6 @@ class ItemHolder extends PluginBase {
             "sqlite" => "sqlite.sql",
         ]);
         $this->database = new SQLiteProvider($connector);
-        $this->testCase();
-    }
-
-    public function testCase() : void {
-        IHolderAPI::register("testcase5", [VanillaItems::DIAMOND()->setCustomName("TEST_CASE1"), VanillaItems::DIAMOND()->setCustomName("2TEST_CASE1")]);
-        IHolderAPI::get("testcase5", function (array|Item|null $itemData) {
-            var_dump($itemData);
-        });
     }
 
     public function getDatabase() : SQLiteProvider {
